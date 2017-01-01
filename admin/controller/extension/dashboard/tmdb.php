06ed33a0cf50d3383f516bbd36b25216c3a092dc
Michel Lima <michelpl@yahoo.com.br>
2017-01-01 21:58:15 -0200

    Limpando o arquivo do controller.

--- admin/controller/extension/dashboard/tmdb.php
+++ admin/controller/extension/dashboard/tmdb.php
@@ -105,41 +105,7 @@ class ControllerExtensionDashboardTmdb extends Controller {
 
     public function dashboard() {
         $this->load->language('extension/dashboard/tmdb');
-
         $data['heading_title'] = $this->language->get('heading_title');
-
-        /* $data['text_no_results'] = $this->language->get('text_no_results');
-
-          $data['token'] = $this->session->data['token'];
-
-          $data['activities'] = array();
-
-          $this->load->model('report/activity');
-
-          $results = $this->model_report_activity->getActivities();
-
-          foreach ($results as $result) {
-          $comment = vsprintf($this->language->get('text_' . $result['key']), json_decode($result['data'], true));
-
-          $find = array(
-          'customer_id=',
-          'order_id=',
-          'affiliate_id=',
-          'return_id='
-          );
-
-          $replace = array(
-          $this->url->link('customer/customer/edit', 'token=' . $this->session->data['token'] . '&customer_id=', true),
-          $this->url->link('tmdb/order/info', 'token=' . $this->session->data['token'] . '&order_id=', true),
-          $this->url->link('marketing/affiliate/edit', 'token=' . $this->session->data['token'] . '&affiliate_id=', true),
-          $this->url->link('tmdb/return/edit', 'token=' . $this->session->data['token'] . '&return_id=', true)
-          );
-
-          $data['activities'][] = array(
-          'comment'    => str_replace($find, $replace, $comment),
-          'date_added' => date($this->language->get('datetime_format'), strtotime($result['date_added']))
-          );
-          } */
         return $this->load->view('extension/dashboard/tmdb_info', $data);
     }
 
